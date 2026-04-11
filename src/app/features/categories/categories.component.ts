884b59756af1105faba306ccad9d7a690b5f63e5
Ibrahim-Hassan74 <ih3046376@gmail.com>
2026-04-11 08:30:00 +0200

    feat(categories): translate categories components

--- src/app/features/categories/categories.component.ts
+++ src/app/features/categories/categories.component.ts
@@ -1,18 +1,20 @@
-import {
-  Component,
-  CUSTOM_ELEMENTS_SCHEMA,
-  input,
-} from '@angular/core';
+import { Component, inject, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { CategoriesBrands } from '../../shared/models/categories-brands';
 import { CategoryCardComponent } from './category-card/category-card.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { LanguageService } from '../../core/services/language/language.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-categories',
-  imports: [CategoryCardComponent],
+  standalone: true,
+  imports: [CommonModule, CategoryCardComponent, TranslateModule],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class CategoriesComponent {
   categoriesBrands = input.required<CategoriesBrands[]>();
+  languageService = inject(LanguageService);
 }
--- src/app/features/categories/category-card/category-card.component.ts
+++ src/app/features/categories/category-card/category-card.component.ts
@@ -1,16 +1,21 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, input, computed } from '@angular/core';
+import { Component, computed, input, inject, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { CategoriesBrands } from '../../../shared/models/categories-brands';
-
 import { RouterModule } from '@angular/router';
+import { LanguageService } from '../../../core/services/language/language.service';
+import { TranslateModule } from '@ngx-translate/core';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-category-card',
-  imports: [RouterModule],
+  standalone: true,
+  imports: [CommonModule, RouterModule, LucideAngularModule, TranslateModule],
   templateUrl: './category-card.component.html',
   styleUrl: './category-card.component.scss',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class CategoryCardComponent {
   cat = input.required<CategoriesBrands>();
+  languageService = inject(LanguageService);
   shouldLoop = computed(() => (this.cat().brandResponse?.length || 0) > 4);
 }
